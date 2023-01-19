81777b264b9eedf00eac8a60c0946788f5f412a0
alix <alix@kronikle.eu>
2023-01-19 13:28:49 +0100

    ajout pr-1

--- layouts/app.vue
+++ layouts/app.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <SideBar class="fixed top-20"></SideBar>
-    <div class="h-20 mt-20 pl-20 lg:pl-60">
+    <div class="h-20 mt-20 pl-20 pr-1 lg:pl-60">
       <slot />
     </div>
   </div>
